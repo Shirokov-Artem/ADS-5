88b45f1bf3fc3ebca928a7b9daae301d321fe0b3
Артем Широков <125153836+Shirokov-Artem@users.noreply.github.com>
2023-03-23 23:41:32 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,81 @@
 // Copyright 2021 NNTU-CS
 #include <string>
 #include <map>
+#include <stack>
 #include "tstack.h"
 
 std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+    std::map<char, int> priority = {{'*', 2}, {'/', 2}, {'+', 1}, {'-', 1}, {'(', 0}};
+    std::string result = "";
+    std::stack<char> stack;
+    for (int i = 0; i < inf.size(); i++) {
+        char c = inf[i];
+        if (isdigit(c)) {
+            result += c;
+            // если следующий символ - не цифра, то добавляем пробел
+            if (i + 1 < inf.size() && !isdigit(inf[i + 1])) {
+                result += ' ';
+            }
+        } else if (c == '(') {
+            stack.push(c);
+        } else if (c == ')') {
+            while (stack.top() != '(') {
+                result += ' ';
+                result += stack.top();
+                stack.pop();
+            }
+            stack.pop();
+        } else {
+            while (!stack.empty() && priority[c] <= priority[stack.top()]) {
+                result += ' ';
+                result += stack.top();
+                stack.pop();
+            }
+            result += ' ';
+            stack.push(c);
+        }
+    }
+    while (!stack.empty()) {
+        result += ' ';
+        result += stack.top();
+        stack.pop();
+    }
+    return result;
 }
 
 int eval(std::string pref) {
-  // добавьте код
-  return 0;
+    std::stack<int> stack;
+    for (int i = 0; i < pref.size(); i++) {
+        char c = pref[i];
+        if (isdigit(c)) {
+            int num = c - '0';
+            while (i + 1 < pref.size() && isdigit(pref[i + 1])) {
+                num = num * 10 + (pref[i + 1] - '0');
+                i++;
+            }
+            stack.push(num);
+        } else if (c == ' ') {
+            continue;
+        } else {
+            int b = stack.top();
+            stack.pop();
+            int a = stack.top();
+            stack.pop();
+            switch (c) {
+                case '+':
+                    stack.push(a + b);
+                    break;
+                case '-':
+                    stack.push(a - b);
+                    break;
+                case '*':
+                    stack.push(a * b);
+                    break;
+                case '/':
+                    stack.push(a / b);
+                    break;
+            }
+        }
+    }
+    return stack.top();
 }
